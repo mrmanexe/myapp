093f975de7f22b923611d0ed7d8b61562fb40caf
tmcokezero <tmcokezero@gmail.com>
2025-05-15 19:14:07 +0000

    boi waht the helly

--- deploy.sh
+++ deploy.sh
@@ -23,7 +23,7 @@ echo "Confirmed on the main branch."
 
 # Step 2: Build the Flutter web app with the correct base href
 echo "Building the Flutter web app with base href /myappRepo/..."
-flutter build web --release --base-href="/mrmanexe.github.io/SecureSlumber/" #change '/myappRepo/' to the name of the github repo of your project
+flutter build web --release --base-href="/SecureSlumber/" #change '/myappRepo/' to the name of the github repo of your project
 
 # Step 3: Verify the build was successful
 if [ ! -d "build/web" ]; then
